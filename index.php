303b994627275c64abc80bf43a2d22c05961871b
nora <noradasilveira0@gmail.com>
2022-08-26 14:17:13 +0000

    modification partie text

--- index.php
+++ index.php
@@ -28,15 +28,15 @@ if(isset($_POST['send'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Complete Responsive Personal Portfolio Website Design</title>
+   <title>da SILVEIRA Nora Portfolio</title>
 
-   <!-- font awesome cdn link  -->
+   <!-- Lien font awesome cdn  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 
-   <!-- aos css link  -->
+   <!-- Lien aos css   -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
 
-   <!-- custom css file link  -->
+   <!-- Lien vers le fichier css  -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -57,7 +57,7 @@ if(isset($message)){
 
 ?>
 
-<!-- header section starts  -->
+<!-- Debut de la section header  -->
 
 <header class="header">
 
@@ -66,11 +66,11 @@ if(isset($message)){
    <a href="#home" class="logo">Portfolio</a>
 
    <nav class="navbar">
-      <a href="#home" class="active">home</a>
-      <a href="#about">about</a>
-      <a href="#services">services</a>
-      <a href="#portfolio">portfolio</a>
-      <a href="#contact">contact</a>
+      <a href="#home" class="active">accueil</a>
+      <a href="#about">A propos</a>
+      <a href="#services">Services</a>
+      <a href="#portfolio">Portfolio</a>
+      <a href="#contact">Contacts</a>
    </nav>
 
    <div class="follow">
@@ -83,9 +83,9 @@ if(isset($message)){
 
 </header>
 
-<!-- header section ends -->
+<!-- Fin de la section header -->
 
-<!-- home section starts  -->
+<!-- Debut de la section Accueil  -->
 
 <section class="home" id="home">
 
@@ -94,72 +94,92 @@ if(isset($message)){
    </div>
 
    <div class="content">
-      <h3 data-aos="fade-up">hi, i am nora</h3>
-      <span data-aos="fade-up">web designer & developer</span>
-      <p data-aos="fade-up">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis labore recusandae minima molestias aliquam nostrum.</p>
-      <a data-aos="fade-up" href="#about" class="btn">about me</a>
+      <h3 data-aos="fade-up">Salut, je suis Nora</h3>
+      <span data-aos="fade-up">Conseiller clientèle & web designer-developpeur Junior</span>
+      <p data-aos="fade-up">
+         Je suis developpeur junior en fin de formation, je suis motivée,
+         passionnée, et avide d'apprentissage. Developpeur professionnel en 
+         devenir que suis, je combine mon savoir faire en codage à mon expéreience 
+         en Marketing pour trouver le juste milieu entre créativité, pragmatisme 
+         et sens commercial. En divers j'aime voyager et découvrir, mes passe temps 
+         favoris sont la musique, la documentation et la mode. N'hesitez pas à 
+         me contacter pour vos différents projets.
+      </p>
+      <a data-aos="fade-up" href="#about" class="btn">A propos de moi</a>
    </div>
 
 </section>
 
-<!-- home section ends -->
+<!-- fin de la section Accueil -->
 
-<!-- about section starts  -->
+<!-- Début de la section A propos  -->
 
 <section class="about" id="about">
 
-   <h1 class="heading" data-aos="fade-up"> <span>biography</span> </h1>
+   <h1 class="heading" data-aos="fade-up"> <span>biographie</span> </h1>
 
    <div class="biography">
 
       <p data-aos="fade-up">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis tenetur amet ab optio minus est error libero adipisci numquam ratione.</p>
 
       <div class="bio">
-         <h3 data-aos="zoom-in"> <span>name : </span> sana shaikh </h3>
-         <h3 data-aos="zoom-in"> <span>email : </span> sanashaikh@gmail.com </h3>
-         <h3 data-aos="zoom-in"> <span>address : </span> mumbai, india </h3>
-         <h3 data-aos="zoom-in"> <span>phone : </span> +123-456-7890 </h3>
-         <h3 data-aos="zoom-in"> <span>age : </span> 22 years </h3>
-         <h3 data-aos="zoom-in"> <span>experience : </span> 2+ years </h3>
+         <h3 data-aos="zoom-in"> <span>Nom : </span> Nora da SILVEIRA </h3>
+         <h3 data-aos="zoom-in"> <span>E-mail : </span> noradasilveira0@gmail.com </h3>
+         <h3 data-aos="zoom-in"> <span>Adresse : </span> Lome, TOGO </h3>
+         <h3 data-aos="zoom-in"> <span>Tel : </span> +228 97017372 </h3>
+         <h3 data-aos="zoom-in"> <span>Age : </span> 27 ans </h3>
+         <h3 data-aos="zoom-in"> <span>Experiences : </span> 6 mois</h3>
       </div>
 
-      <a href="#" class="btn" data-aos="fade-up">download CV</a>
+      <a href="#" class="btn" download="" data-aos="fade-up">Telecharger CV</a>
 
    </div>
    
    <div class="skills" data-aos="fade-up">
 
-      <h1 class="heading"> <span>skills</span> </h1>
+      <h1 class="heading"> <span>Compétences</span> </h1>
 
       <div class="progress">
-         <div class="bar" data-aos="fade-left"> <h3><span>HTML</span> <span>95%</span></h3> </div>
-         <div class="bar" data-aos="fade-right"> <h3><span>CSS</span> <span>80%</span></h3> </div>
-         <div class="bar" data-aos="fade-left"> <h3><span>JavaScript</span> <span>65%</span></h3> </div>
-         <div class="bar" data-aos="fade-right"> <h3><span>PHP</span> <span>80%</span></h3> </div>
+         <div class="bar" data-aos="fade-left"> <h3><span>HTML</span> <span>70%</span></h3> </div>
+         <div class="bar" data-aos="fade-right"> <h3><span>CSS</span> <span>50%</span></h3> </div>
+         <div class="bar" data-aos="fade-left"> <h3><span>JavaScript</span> <span>30%</span></h3> </div>
+         <div class="bar" data-aos="fade-right"> <h3><span>PHP</span> <span>20%</span></h3> </div>
       </div>
 
    </div>
 
    <div class="edu-exp">
 
-      <h1 class="heading" data-aos="fade-up"> <span>education & experience</span> </h1>
+      <h1 class="heading" data-aos="fade-up"> <span>Parcours & experiences</span> </h1>
 
       <div class="row">
 
          <div class="box-container">
 
-            <h3 class="title" data-aos="fade-right">education</h3>
+            <h3 class="title" data-aos="fade-right">Parcours</h3>
 
             <div class="box" data-aos="fade-right">
-               <span>( 2019 - 2020 )</span>
-               <h3>web designer</h3>
-               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda, eaque sequi repellat natus quia.</p>
+               <span>( Fév 2022 - Sept 2022 )</span>
+               <h3>ENERGY GENERATION</h3>
+               <p>
+                  Est une organisation panafricaine qui encourage les jeunes Africains à relever les défis les plus pressants de leur génération.
+               </p>
             </div>
 
             <div class="box" data-aos="fade-right">
-               <span>( 2020 - 2021 )</span>
-               <h3>web developer</h3>
-               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda, eaque sequi repellat natus quia.</p>
+               <span>( 2019 - 2021 )</span>
+               <h3>PCCI - Togo</h3>
+               <p>
+                  Mutionationale opérant dans la gestion externalisée de la relation client
+               </p>
+            </div>
+
+            <div class="box" data-aos="fade-right">
+               <span>( 2018 - 2019 )</span>
+               <h3>ADKontact</h3>
+               <p>
+                  Entreprise spécialisée dans la gestion de la relation clientèle et la vente à distance de contrats
+               </p>
             </div>
 
             <div class="box" data-aos="fade-right">
@@ -175,14 +195,21 @@ if(isset($message)){
             <h3 class="title" data-aos="fade-left">experience</h3>
 
             <div class="box" data-aos="fade-left">
-               <span>( 2019 - 2020 )</span>
-               <h3>front-end developer</h3>
+               <!-- Description du parcours a ENERGY GENERATION -->
+               <span>( Fév 2022 - Sept 2022 )</span>
+               <h3>FORMATION D-CLIC DWWM</h3>
                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda, eaque sequi repellat natus quia.</p>
             </div>
 
             <div class="box" data-aos="fade-left">
                <span>( 2020 - 2021 )</span>
-               <h3>back-end developer</h3>
+               <h3>Conseiller clientèle</h3>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda, eaque sequi repellat natus quia.</p>
+            </div>
+
+            <div class="box" data-aos="fade-left">
+               <span>( 2018 - 2019 )</span>
+               <h3>Télévendeur & Conseiller clientèle</h3>
                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat assumenda, eaque sequi repellat natus quia.</p>
             </div>
 
@@ -200,9 +227,9 @@ if(isset($message)){
 
 </section>
 
-<!-- about section ends -->
+<!-- début de la section A propos -->
 
-<!-- services section starts  -->
+<!-- début de la section services -->
 
 <section class="services" id="services">
 
@@ -212,7 +239,7 @@ if(isset($message)){
 
       <div class="box" data-aos="zoom-in">
          <i class="fas fa-code"></i>
-         <h3>web development</h3>
+         <h3>developpement web</h3>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium nostrum rem ipsa.</p>
       </div>
 
@@ -236,7 +263,7 @@ if(isset($message)){
 
       <div class="box" data-aos="zoom-in">
          <i class="fas fa-bullhorn"></i>
-         <h3>digital marketing</h3>
+         <h3>Marketing digital</h3>
          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, placeat veritatis accusantium nostrum rem ipsa.</p>
       </div>
 
@@ -250,9 +277,9 @@ if(isset($message)){
 
 </section>
 
-<!-- services section ends -->
+<!-- fin de la section services -->
 
-<!-- portfolio section starts  -->
+<!-- début de la section portfolio  -->
 
 <section class="portfolio" id="portfolio">
 
@@ -300,13 +327,13 @@ if(isset($message)){
 
 </section>
 
-<!-- portfolio section ends -->
+<!-- fin de la section portfolio -->
 
-<!-- contact section starts  -->
+<!-- début la section contact  -->
 
 <section class="contact" id="contact">
 
-   <h1 class="heading" data-aos="fade-up"> <span>contact me</span> </h1>
+   <h1 class="heading" data-aos="fade-up"> <span>me contacter</span> </h1>
 
    <form action="" method="post">
       <div class="flex">
@@ -322,31 +349,29 @@ if(isset($message)){
 
       <div class="box" data-aos="zoom-in">
          <i class="fas fa-phone"></i>
-         <h3>phone</h3>
-         <p>+123-456-7890</p>
+         <h3>téléphone</h3>
+         <p>+228 97-01-73-72</p>
       </div>
 
       <div class="box" data-aos="zoom-in">
          <i class="fas fa-envelope"></i>
          <h3>email</h3>
-         <p>sanashaikh@gmail.com</p>
+         <p>noradasilveira0@gmail.com<</p>
       </div>
 
       <div class="box" data-aos="zoom-in">
          <i class="fas fa-map-marker-alt"></i>
-         <h3>address</h3>
-         <p>mumbai, india - 400104</p>
+         <h3>adresse</h3>
+         <p>Lomé, Togo - 400104</p>
       </div>
 
    </div>
 
 </section>
 
-<!-- contact section ends -->
+<!-- fin de la section contact  -->
 
-<div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>mr. web designer</span> </div>
-
-
+<div class="credit"> &copy; copyright @ <?php echo date('Y'); ?> by <span>Nora da SILVEIRA</span> </div>
 
 
 
@@ -357,10 +382,12 @@ if(isset($message)){
 
 
 
-<!-- custom js file link  -->
+
+
+<!-- lien vers un fichier js personnalisé  -->
 <script src="js/script.js"></script>
 
-<!-- aos js link  -->
+<!-- lien vers aos js  -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 
 <script>
